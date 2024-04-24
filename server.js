784699a1c1241e1cf1910407e2eacbe5fb499ffa
VENDABLEPATH <wianjacobs04@gmail.com>
2024-04-24 13:43:34 +0200

    Added OAuth using Passport.js

--- server.js
+++ server.js
@@ -1,17 +1,70 @@
 const fs = require('fs');
 const path = require('path');
+
+require('dotenv').config();
+
 const https = require('https');
 const express = require('express');
+const passport = require('passport');
+const  { Strategy } = require('passport-google-oauth20');
 
 const helmet = require('helmet');
 
 const PORT = 3000;
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
+};
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+    console.log(`Google profile:`, profile);
+    done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
-app.get('/secret', (req, res) => {
+
+function checkLoggedIn(req, res, next){
+    const isLoggedIn = true; // TODO
+    if (!isLoggedIn){
+        return res.status(401).json({error: "User is not logged in"});
+    };
+    next();
+};
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+    failurRedirect: '/failure',
+    successRedirect: '/',
+    session: false
+}), (req, res) => {
+    console.log('Google called us back.');
+});
+
+app.get('/failure', (req, res) => {
+    return res.send('Failed to log in.')
+});
+
+app.get('/auth/logout', (req, res) => {
+    // TODO
+});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your personal secret value is 42');
 });
 
